14e92d13a6aca9c8a745380dcdbdea0273007051
yhx-12243 <yhx0706@163.com>
2017-10-01 07:46:45 +0800

    prec0

--- additional_files/_vti_cnf/basic.js
+++ additional_files/_vti_cnf/basic.js
@@ -1,9 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|23 Aug 2017 08:30:20 -0000
-vti_extenderversion:SR|12.0.0.6211
+vti_timelastmodified:TR|24 Aug 2017 09:48:48 -0000
+vti_extenderversion:SR|12.0.0.6500
 vti_author:SR|st23007\\Administrator
 vti_modifiedby:SR|st23007\\Administrator
 vti_timecreated:TR|01 Jan 2000 09:07:34 -0000
 vti_backlinkinfo:VX|
-vti_cacheddtm:TX|23 Aug 2017 08:30:20 -0000
-vti_filesize:IR|1533
+vti_nexttolasttimemodified:TW|24 Aug 2017 09:46:46 -0000
+vti_cacheddtm:TX|24 Aug 2017 09:48:48 -0000
+vti_filesize:IR|2421
--- additional_files/_vti_cnf/file.js
+++ additional_files/_vti_cnf/file.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|23 Aug 2017 10:04:10 -0000
-vti_extenderversion:SR|12.0.0.6211
+vti_timelastmodified:TR|27 Aug 2017 10:39:53 -0000
+vti_extenderversion:SR|12.0.0.6500
 vti_author:SR|st23007\\Administrator
 vti_modifiedby:SR|st23007\\Administrator
 vti_timecreated:TR|23 Aug 2017 05:29:48 -0000
-vti_nexttolasttimemodified:TW|23 Aug 2017 09:58:05 -0000
+vti_nexttolasttimemodified:TW|27 Aug 2017 10:39:30 -0000
 vti_backlinkinfo:VX|
-vti_cacheddtm:TX|23 Aug 2017 10:04:10 -0000
-vti_filesize:IR|1344
+vti_cacheddtm:TX|27 Aug 2017 10:39:53 -0000
+vti_filesize:IR|1648
