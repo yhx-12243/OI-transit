c4d85344df3547b7b1d152b3a25425cd2be1d821
yhx-12243 <yhx0706@163.com>
2017-12-08 08:03:22 +0800

    OI-transit update 12-8
    * Add new records.
    * Add the rocket to top in templates.
    * Add some styles.

--- additional_files/basic.js
+++ additional_files/basic.js
@@ -1,155 +1,97 @@
-function displaytime(v){
-    var now = new Date()
-    var yr = now.getFullYear();
-    var mt = now.getMonth() + 1;
-    var dy = now.getDate();
-    var hr = now.getHours();
-    var mn = now.getMinutes();
-    var sc = now.getSeconds();
-    mn = mn < 10 ? "0" + mn : mn;
-    sc = sc < 10 ? "0" + sc : sc;
-    var res = "当前时间：" + yr + "-" + mt + "-" + dy + " " + hr + ":" + mn + ":" + sc;
-    v.innerHTML = res;
-}
+var DisplayDict, NormDict, SiteDict, xhr, xs;
 
-function getOJ(s){
-	var t = s.substr(1), arr = t.split("&"), i, brr, res = "";
-	for(i in arr){
-		brr = arr[i].split("=");
-		if(brr[0] === "selOJ"){
-			if(brr.length > 1)
-				res = brr[1];
-			break;
-		}
+function init(){
+	DisplayDict = {'': '全部', 'Luogu': '洛谷', 'SPOJ': 'Sphere OJ', 'Local': '本地', "Unknown": "这些题的 OJ 太高级了，连 scx 都不知道，快去问一问大佬们吧"};
+	NormDict = {'lydsy': 'Lydsy', 'lg': 'Luogu', 'vijos': 'Vijos', 'hdu': 'HDU', 'poj': 'POJ', 'uoj': 'UOJ', 'loj': 'LibreOJ', 'simpleoj': 'SimpleOJ', 'cf': 'Codeforces', 'cc': 'Codechef', 'spoj': 'SPOJ'};
+	SiteDict = {
+		'lydsy': [[/\d+/], "http://www.lydsy.com/JudgeOnline/problem.php?id=@0"],
+		"lg": [[/[TU]?\d+/], "https://www.luogu.org/problemnew/show/@0"],
+		"hdu": [[/\d+/], "http://acm.hdu.edu.cn/showproblem.php?pid=@0"],
+		"poj": [[/\d+/], "http://poj.org/problem?id=@0"],
+		"uoj": [[/\d+/], "http://uoj.ac/problem/@0"],
+		"loj": [[/\d+/], "https://loj.ac/problem/@0"],
+		"simpleoj": [[/\d+/], "http://10.49.27.23/problem?id=@0"],
+		"cf": [[/\d+/, /[A-G]\d*/], "http://codeforces.com/contest/@0/problem/@1"],
+		"cc": [[/\w+/], "https://www.codechef.com/problems/@0/"],
+		"spoj": [[/\w+/], "http://www.spoj.com/problems/@0/"]
 	}
-	return res;
+	xhr = new XMLHttpRequest();
+	xs = new XMLSerializer();
 }
 
-function getPage(s){
-	var t = s.substr(1), arr = t.split("&"), i, brr, res = "1";
-	for(i in arr){
-		brr = arr[i].split("=");
-		if(brr[0] === "page"){
-			if(brr.length > 1 && brr[1] !== "")
-				res = brr[1];
-			break;
-		}
+function getTime(v){
+	var now = new Date(),
+	year = now.getFullYear(),
+	month = now.getMonth() + 1,
+	day = now.getDate(),
+	hour = now.getHours(),
+	minute = now.getMinutes(),
+	second = now.getSeconds();
+	return '当前时间: ' + year + '-' + month + '-' + day + ' ' + hour + ':' +
+	(minute < 10 ? '0' : '') + minute + ':' +
+	(second < 10 ? '0' : '') + second;
+}
+
+function parseSearch(key, def){
+	var reg = new RegExp('[?&]' + key + '=([^&]*)(&|$)'),
+	r = window.location.search.match(reg);
+	return r ? unescape(r[1]) : def;
+}
+
+function getScrollTop(){
+	var pos = 0, pos1 = window.document.documentElement.scrollTop, pos2 = window.document.body.scrollTop;
+	if(pos1 !== undefined) pos += pos1;
+	if(pos2 !== undefined) pos += pos2;
+	if(pos1 === undefined && pos2 === undefined){
+		if(window.scrollY !== undefined) return window.scrollY;
+		else alert("你正在使用过时的浏览器，OI-transit 暂不支持。请升级浏览器以获得更好的体验！");
 	}
-	return res;
+	return pos;
 }
 
-function getFull(s){
-	switch(s){
-		case "":
-			return "全部";
-		case "Luogu":
-			return "洛谷";
-		case "SPOJ":
-			return "Sphere OJ"
-		case "Local":
-			return "本地测试";
-		case "Unknown":
-			return "这些题的 OJ 太高级了，连 scx 都不知道，快去问一问大佬们吧";
-		default:
-			return s;
-	}
-}
+function getDisplay(s) {return DisplayDict.hasOwnProperty(s) ? DisplayDict[s] : s;}
 
-function testNorm(s){
-	switch(s){
-		case "lydsy":
-			return "Lydsy";
-		case "lg":
-			return "Luogu";
-		case "vijos":
-			return "Vijos";
-		case "hdu":
-			return "HDU";
-		case "poj":
-			return "POJ";
-		case "uoj":
-			return "UOJ";
-		case "loj":
-			return "LibreOJ";
-		case "simpleoj":
-			return "SimpleOJ";
-		case "cf":
-			return "Codeforces";
-		case "cc":
-			return "Codechef";
-		case "spoj":
-			return "SPOJ";
-		default:
-			return "";
-	}
-}
+function getNorm(s) {return NormDict.hasOwnProperty(s) ? NormDict[s] : null;}
 
-function getSite(s){
-	switch(s){
-		case "lydsy":
-			return [[/\d+/], "http://www.lydsy.com/JudgeOnline/problem.php?id=@0"];
-		case "lg":
-			return [[/[TU]?\d+/], "https://www.luogu.org/problemnew/show/@0"];
-		case "hdu":
-			return [[/\d+/], "http://acm.hdu.edu.cn/showproblem.php?pid=@0"];
-		case "poj":
-			return [[/\d+/], "http://poj.org/problem?id=@0"];
-		case "uoj":
-			return [[/\d+/], "http://uoj.ac/problem/@0"];
-		case "loj":
-			return [[/\d+/], "https://loj.ac/problem/@0"];
-		case "simpleoj":
-			return [[/\d+/], "http://10.49.27.23/problem?id=@0"];
-		case "cf":
-			return [[/\d+/, /[A-G]\d*/], "http://codeforces.com/contest/@0/problem/@1"];
-		case "cc":
-			return [[/\w+/], "https://www.codechef.com/problems/@0/"];
-		case "spoj":
-			return [[/\w+/], "http://www.spoj.com/problems/@0/"];
-		default:
-			return null;
-	}
-}
+function getSite(s) {return SiteDict.hasOwnProperty(s) ? SiteDict[s] : null;}
 
-function Matching(jF, pF){
-	var brr, i = 0, j = 0, ls = "";
-	if(jF === "") return true;
-	brr = pF.split(";");
+function Matching(ptrn, s){
+	var brr, i, j, leader;
+	if(!ptrn) return true;
+	brr = s.split(';');
 	for(i in brr){
-		j = brr[i].search(/[^a-z]/);
-		if(j < 0) continue;
-		ls = brr[i].substr(0, j);
-		if(jF === "Unknown"){
-			if(j > 0 && testNorm(ls) === "") return true;
-		}else if(jF === "Local"){
-			if(j === 0) return true;
+		if(j = brr[i].search(/[^a-z]/), j < 0) continue;
+		leader = brr[i].substr(0, j);
+		if(ptrn === 'Unknown'){
+			if(j > 0 && !getNorm(leader)) return true;
+		}else if(ptrn === 'Local'){
+			if(!j) return true;
 		}else
-			if(testNorm(ls) === jF) return true;
+			if(getNorm(leader) === ptrn) return true;
 	}
 	return false;
 }
 
-function prToHTML(pr){
-	var brr, i = 0, j = 0, ret = "", failed = false;
-	var sOJ = "", sID = "", siteMethod, regSite, strSite, regResult;
-	brr = pr.split(";");
+function pr2HTML(s){
+	var brr, i, j, ret = '', failed;
+	var sOJ, sID, siteMethod, regSite, strSite, regResult;
+	brr = s.split(';');
 	for(i in brr){
-		j = brr[i].search(/[^a-z]/);
-		if(j <= 0) {ret += brr[i] + ";"; continue;}
+		if(j = brr[i].search(/[^a-z]/), j <= 0) {ret += brr[i] + ';'; continue;}
 		sOJ = brr[i].substr(0, j);
 		sID = brr[i].substr(j);
 		siteMethod = getSite(sOJ);
-		if(siteMethod === null) {ret += brr[i] + ";"; continue;}
+		if(!siteMethod) {ret += brr[i] + ";"; continue;}
 		regSite = siteMethod[0]; 
 		strSite = siteMethod[1];
 		failed = false;
 		for(j in regSite){
 			regResult = sID.match(regSite[j]);
-			if(regResult === null) {failed = true; break;}
+			if(!regResult) {failed = true; break;}
 			strSite = strSite.replace("@" + j, regResult[0]);
 		}
 		if(failed) {ret += brr[i] + ";"; continue;}
-		ret += "<a href=\"" + strSite + "\" target=\"_blank\">" + brr[i] + "</a>;";
+		ret += '<a href="' + strSite + '" target="_blank">' + brr[i] + '</a>;';
 	}
 	return ret.substr(0, ret.length - 1);
 }
--- additional_files/file.js
+++ additional_files/file.js
@@ -1,37 +1,43 @@
-var xh, xs, recarr, tearr, tetiarr, memarr;
-var strBack, xmlBack;
-var strFilter, pageO, pageC, nowId;
+var recarr, tearr, tetiarr, memarr;
+var strBack, xmlBack, nowId;
+var strFilter, pageO, pageC; // global
 
-const RECORDS_PER_PAGE = 50, MEMOS_PER_PAGE = 50;
+const RECORDS_PER_PAGE = 50, MEMOS_PER_PAGE = 50, failed = '今天运气不怎么好，和 scx 喝口茶再试试吧';
 
-const failed = "今天运气不怎么好，和 scx 喝口茶再试试吧";
-
-function LoadRecords(){
-	strBack = "";
-	xh = new XMLHttpRequest();
-	xh.onreadystatechange = read_ok1;
-	xh.open("GET", "records/tot.cfg", true);
-	xh.send(null);
+// ---------------- Records ---------------- //
+function LoadRecordsTo(tar, jan){
+	strBack = '';
+	xhr.onreadystatechange = function (){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200 || xhr.status === 0){
+				strBack = xhr.responseText;
+				DecRecords(tar, jan);
+			}else
+				alert(failed);
+		}
+	}
+	xhr.open('GET', 'records/tot.cfg', true);
+	xhr.send(null);
 }
 
-function DecRecords(){
-	var i = 0, nm = "", info, count = 0, recnum = 0;
+function DecRecords(tar, jan){
+	var i, nm, info, count = 0, recnum = 0;
 	var newRow, C0, C1, C2, C3, C4, C5, C6;
-	recarr = strBack.split("\n");
+	recarr = strBack.split('\n');
 	for(i in recarr)
-		if(recarr[i] !== ""){
-			info = recarr[i].replace(/[\f\n\r\v]/g, "").split("|");
+		if(recarr[i]){
+			info = recarr[i].replace(/[\f\n\r\v]/g, '').split('|');
 			if(info.length !== 5) continue; // invalid record
 			++recnum;
 			nm = info[0];
 			if(Matching(strFilter, nm)){
-				newRow = recTable.tBodies[0].insertRow();
+				newRow = tar.insertRow();
 				C0 = newRow.insertCell();
 				C0.innerHTML = (recnum - 1).toString()
 				C1 = newRow.insertCell();
-				C1.innerHTML = prToHTML(nm);
+				C1.innerHTML = pr2HTML(nm);
 				C2 = newRow.insertCell();
-				C2.innerHTML = "<a href=\"records/" + nm.replace(/#/g, "%23") + ".html\" target=\"_blank\">链接</a>";
+				C2.innerHTML = '<a href="records/' + escape(nm) + '.html" target="_blank">链接</a>';
 				C3 = newRow.insertCell();
 				C3.innerHTML = info[1];
 				C4 = newRow.insertCell();
@@ -40,94 +46,109 @@ function DecRecords(){
 				C5.innerHTML = info[3];
 				C6 = newRow.insertCell();
 				C6.innerHTML = info[4];
-				++count;
-				if(count <= (pageO - 1) * RECORDS_PER_PAGE || pageO * RECORDS_PER_PAGE < count)
-					newRow.style.display = "none";
+				if(++count, count <= (pageO - 1) * RECORDS_PER_PAGE || pageO * RECORDS_PER_PAGE < count)
+					newRow.style.display = 'none';
 			}
 		}
 	for(i = 0; i < count; ++i){
-		C0 = recTable.tBodies[0].rows[i].cells[0];
+		C0 = tar.rows[i].cells[0];
 		C0.innerHTML = (recnum - parseInt(C0.innerHTML)).toString();
 	}
-	if(count)
-		if(strFilter === "") // all records
-			recTotal.innerHTML = "scx: 共做了 " + count + " 道题";
-		else // filtered
-			recTotal.innerHTML = "scx: 当前筛选下共有 " + count + " 道题";
-	else // haven't done any problem (in thes filter)
-		recTotal.innerHTML = "scx: 怎么一道题都还没有啊，快点做题了！";
+	jan.innerHTML = 'scx: ' + (count ? (strFilter ? '当前筛选下共有 ' : '共做了 ') + count + ' 道题' : '怎么一道题都还没有啊，快点做题了！');
 	pageC = Math.ceil(count / RECORDS_PER_PAGE);
 	pageDeals();
 }
 
-function LoadTemplates(){
-	strBack = "";
-	xs = new XMLSerializer();
-	xh = new XMLHttpRequest();
-	xh.onreadystatechange = read_ok2;
-	xh.open("GET", "templates/tot.cfg", true);
-	xh.send(null);
+// ---------------- Templates ----------------- //
+function LoadTemplates(tar){
+	strBack = '';
+	xhr.onreadystatechange = function (){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200 || xhr.status === 0){
+				strBack = xhr.responseText;
+				DecTemplate(); nowId = 0;
+				OpenTemplate(tar);
+			}else
+				alert(failed);
+		}
+	}
+	xhr.open('GET', 'templates/tot.cfg', true);
+	xhr.send(null);
 }
 
 function DecTemplate(){
-	var i, info = "", filename = "";
-	tearr = strBack.split("\n"); tetiarr = []
+	var i;
+	tearr = strBack.split('\n'); tetiarr = [];
 	for(i in tearr)
-		if(tearr[i] != "")
-			tearr[i] = tearr[i].replace(/[\f\n\r\v]/g, "");
+		if(tearr[i])
+			tearr[i] = tearr[i].replace(/[\f\n\r\v]/g, '');
 }
 
-function OpenTemplate(){
+function OpenTemplate(tar){
 	if(nowId >= tearr.length) return;
 	var f = tearr[nowId];
-	if(f === "") return;
-	// xh = new XMLHttpRequest();
-	xh.onreadystatechange = read_ok3;
-	f = "templates/" + f + ".xml";
-	xh.open("GET", f, true);
-	xh.send(null);
+	if(!f) return;
+	xhr.onreadystatechange = function (){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200 || xhr.status === 0){
+				xmlBack = xhr.responseXML;
+				if(xmlBack) Analyze(tar);
+				++nowId; OpenTemplate(tar);
+			}else
+				alert(failed);
+		}
+	}
+	f = 'templates/' + f + '.xml';
+	xhr.open('GET', f, true);
+	xhr.send(null);
 }
 
-function Analyze(){
+function Analyze(tar){
 	var code, inner;
 	var E0, E1;
 
-	tetiarr[nowId] = xmlBack.getElementsByTagName("title")[0].childNodes[0].nodeValue;
-	E0 = document.createElement("h3");
+	tetiarr[nowId] = xmlBack.getElementsByTagName('title')[0].childNodes[0].nodeValue;
+	E0 = document.createElement('h3');
 	E0.id = tearr[nowId];
-	E0.innerHTML = tetiarr[nowId] + " [#" + tearr[nowId] + "]：";
+	E0.innerHTML = tetiarr[nowId] + ' [#' + tearr[nowId] + ']：';
 	templates.appendChild(E0);
-	code = xmlBack.getElementsByTagName("pre")[0];
+	code = xmlBack.getElementsByTagName('pre')[0];
 	
 	if(code.innerXML) inner = code.innerXML;
 	else if(code.xml) inner = code.xml;
-	else
-		// xs = new XMLSerializer();
-		inner = xs.serializeToString(code);
+	else inner = xs.serializeToString(code);
 	
-	E1 = document.createElement("div");
+	E1 = document.createElement('div');
 	E1.innerHTML = inner;
-	templates.appendChild(E1);
+	tar.appendChild(E1);
 }
 
-function LoadMemos(){
-	strBack = "";
-	xh = new XMLHttpRequest();
-	xh.onreadystatechange = read_ok4;
-	xh.open("GET", "memos/tot.cfg", true);
-	xh.send(null);
+// ---------------- Memos ---------------- //
+function LoadMemosTo(tar, jan){
+	strBack = '';
+	xhr.onreadystatechange = function (){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200 || xhr.status === 0){
+				strBack = xhr.responseText;
+				DecMemos(tar, jan);
+			}else
+				alert(failed);
+		}
+	}
+	xhr.open('GET', 'memos/tot.cfg', true);
+	xhr.send(null);
 }
 
-function DecMemos(){
-	var i = 0, j = 0, nm = "", info, count = 0;
+function DecMemos(tar, jan){
+	var i, nm, info, count = 0;
 	var Rows = [], C0, C1, C2, C3, C4, C5, C6;
-	memarr = strBack.split("\n");
+	memarr = strBack.split('\n');
 	for(i in memarr)
-		if(memarr[i] !== ""){
-			info = memarr[i].replace(/[\f\n\r\v]/g, "").split("|");
+		if(memarr[i] !== ''){
+			info = memarr[i].replace(/[\f\n\r\v]/g, '').split('|');
 			if(info.length !== 4) continue; // invalid memo
 			nm = info[0];
-			Rows[i] = document.createElement("tr");
+			Rows[i] = document.createElement('tr');
 			C0 = Rows[i].insertCell();
 			C0.innerHTML = count.toString()
 			C1 = Rows[i].insertCell();
@@ -139,61 +160,18 @@ function DecMemos(){
 			C4.innerHTML = info[2];
 			++count;
 			if(count <= (pageO - 1) * MEMOS_PER_PAGE || pageO * MEMOS_PER_PAGE < count)
-				Rows[i].style.display = "none";
+				Rows[i].style.display = 'none';
 			Rows[i].priority = parseInt(info[3]);
 		}
 	for(i = 0; i < count; ++i){
 		C0 = Rows[i].cells[0];
 		C0.innerHTML = (j = count - parseInt(C0.innerHTML)).toString();
 		C2 = Rows[i].cells[2];
-		C2.innerHTML = "<a href=\"memos/" + j + ".html\" target=\"_blank\">链接</a>";
+		C2.innerHTML = '<a href="memos/' + j + '.html" target="_blank">链接</a>';
 	}
 	Rows.sort(function(a, b) {return b.priority - a.priority});
-	for(i = 0; i < count; ++i)
-		memTable.tBodies[0].appendChild(Rows[i]);
-	memTotal.innerHTML = "scx: 共 " + count + " 份便笺";
-	pageC = Math.ceil(count / RECORDS_PER_PAGE);
+	for(i = 0; i < count; ++i) tar.appendChild(Rows[i]);
+	jan.innerHTML = 'scx: 共 ' + count + ' 份便笺';
+	pageC = Math.ceil(count / MEMOS_PER_PAGE);
 	pageDeals();
-}
-
-function read_ok1(){
-	if(xh.readyState === 4){
-		if(xh.status === 200 || xh.status === 0){
-			strBack = xh.responseText;
-			DecRecords();
-		}else
-			alert(failed);
-	}
-}
-
-function read_ok2(){
-	if(xh.readyState === 4){
-		if(xh.status === 200 || xh.status === 0){
-			strBack = xh.responseText;
-			DecTemplate();
-			nowId = 0; OpenTemplate();
-		}else
-			alert(failed);
-	}
-}
-
-function read_ok3(){
-	if(xh.readyState === 4){
-		if(xh.status === 200 || xh.status === 0){
-			xmlBack = xh.responseXML;
-			if(xmlBack) Analyze();
-			++nowId; OpenTemplate();
-		}else
-			alert(failed);
-	}
-}
-
-function read_ok4(){
-	if(xh.readyState === 4){
-		if(xh.status === 200 || xh.status === 0){
-			strBack = xh.responseText;
-			DecMemos();
-		}else
-			alert(failed);
-	}
 }
