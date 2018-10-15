4358859a24d2be3a610b1554fa8c18c9ae51200e
yhx-12243 <yhx0706@163.com>
2018-10-15 19:30:24 +0800

    OI-transit update 10-15
    * Add 11 records.
    * Add 1 memo.
    * Fix the UI of encrypted memo and update the encryption algorithm.
    * Update 2 templates.

--- additional_files/sh_main.js
+++ additional_files/sh_main.js
@@ -4,7 +4,6 @@ syntax_highlight_cpp = [
 		[ /\b(?:class|const_cast|delete|dynamic_cast|explicit|false|friend|inline|mutable|namespace|new|operator|private|protected|public|reinterpret_cast|static_cast|template|this|throw|true|try|typeid|typename|using|virtual)\b/g, "sh_keyword", -1],
 		[ /\/\/\//g, "sh_comment", 1],
 		[ /\/\//g, "sh_comment", 7],
-		[ /\/\*\*/g, "sh_comment", 8],
 		[ /\/\*/g, "sh_comment", 9],
 		[ /(\bstruct)([ \t]+)([A-Za-z0-9_]+)/g, ["sh_keyword", "sh_normal", "sh_classname"], -1],
 		[ /^[ \t]*#(?:[ \t]*include)/g, "sh_preproc", 10, 1],
@@ -17,8 +16,8 @@ syntax_highlight_cpp = [
 		[ /~|!|%|\^|\*|\(|\)|-|\+|=|\[|\]|\\|:|;|,|\.|\/|\?|&|<|>|\|/g, "sh_symbol", -1],
 		[ /\{|\}/g, "sh_cbracket", -1],
 		[ /(?:[A-Za-z]|_)[A-Za-z0-9_]*(?=[ \t]*\()/g, "sh_function", -1],
-		[ /([A-Za-z](?:[^`~!@#$%&*()_=+{}|;:",<.>\/?'\\[\]\^\-\s]|[_])*)((?:<.*>)?)(\s+(?=[*&]*[A-Za-z][^`~!@#$%&*()_=+{}|;:",<.>\/?'\\[\]\^\-\s]*\s*[`~!@#$%&*()_=+{}|;:",<.>\/?'\\[\]\^\-\[\]]+))/g, ["sh_usertype", "sh_usertype", "sh_normal"], -1]
-	],
+		[ /([A-Za-z](?:[^`~!@#$%&*()_=+{}|;:",<.>\/?'\\[\]\^\-\s]|[_])*)(<)(.*)(>)(\s+(?=[*&]*[A-Za-z][^`~!@#$%&*()_=+{}|;:",<.>\/?'\\[\]\^\-\s]*\s*[`~!@#$%&*()_=+{}|;:",<.>\/?'\\[\]\^\-\[\]]+))/g, ["sh_type", "sh_symbol", "sh_type", "sh_symbol", "sh_normal"], -1]
+	], // 0
 	[
 		[ /$/g, null, -2],
 		[ /(?:<?)[A-Za-z0-9_\.\/\-_~]+@[A-Za-z0-9_\.\/\-_~]+(?:>?)|(?:<?)[A-Za-z0-9_]+:\/\/[A-Za-z0-9_\.\/\-_~]+(?:>?)/g, "sh_url", -1],
@@ -26,34 +25,34 @@ syntax_highlight_cpp = [
 		[ /@[A-Za-z]+/g, "sh_type", -1],
 		[ /([^=" \t>]+)([ \t]*)(=?)/g, ["sh_type", "sh_normal", "sh_symbol"], -1],
 		[ /"/g, "sh_string", 3]
-	],
-	[],
+	], // 1
+	[], // 2
 	[
 		[ /\\(?:\\|")/g, null, -1],
 		[ /"/g, "sh_string", -2]
-	],
+	], // 3
 	[
 		[ />/g, "sh_preproc", -2],
 		[ /([^=" \t>]+)([ \t]*)(=?)/g, ["sh_type", "sh_normal", "sh_symbol"], -1],
 		[ /"/g,"sh_string", 3]
-	],
-	[],
+	], // 4
+	[], // 5
 	[
 		[ /(?:\/)?>/g, "sh_keyword",-2],
 		[ /([^=" \t>]+)([ \t]*)(=?)/g, ["sh_type", "sh_normal", "sh_symbol"], -1],
 		[ /"/g,"sh_string", 3]
-	],
+	], // 6
 	[
 		[ /$/g, null, -2]
-	],
+	], // 7
 	[
 		[ /\*\//g, "sh_comment", -2],
 		[ /&(?:[A-Za-z0-9]+);/g, "sh_preproc", -1],
 		[ /@[A-Za-z]+/g, "sh_type", -1],
-	],
+	], // 8
 	[
 		[ /\*\//g, "sh_comment", -2],
-	],
+	], // 9
 	[
 		[ /$/g, null, -2],
 		[ /</g, "sh_string", 11],
@@ -62,250 +61,486 @@ syntax_highlight_cpp = [
 		[ /\/\//g, "sh_comment", 7],
 		[ /\/\*\*/g, "sh_comment", 8],
 		[ /\/\*/g, "sh_comment", 9]
-	],
+	], // 10
 	[
 		[ /$/g, null, -2],
 		[ />/g, "sh_string", -2]
-	],
+	], // 11
 	[
 		[ /$/g, null, -2],
 		[ /\\(?:\\|")/g, null, -1],
 		[ /"/g, "sh_string", -2]
-	],
+	], // 12
 	[
 		[ /"/g, "sh_string", -2],
 		[ /\\./g, "sh_specialchar", -1]
-	],
+	], // 13
 	[
 		[ /'/g, "sh_string", -2],
 		[ /\\./g, "sh_specialchar", -1]
-	]
+	] // 14
 ];
 
-function sh_isEmailAddress(a) {
-	return /^mailto:/.test(a) ? !1 : -1 !== a.indexOf("@")
+function sh_isEmailAddress(url) {
+	if (/^mailto:/.test(url)) return false;
+	return url.indexOf('@') !== -1;
 }
 
-function sh_setHref(a, b, c) {
-	var d = c.substring(a[b - 2].pos, a[b - 1].pos);
-	d.length >= 2 && "<" === d.charAt(0) && ">" === d.charAt(d.length - 1) && (d = d.substr(1, d.length - 2)),
-	sh_isEmailAddress(d) && (d = "mailto:" + d),
-	a[b - 2].node.href = d
-}
-
-function sh_konquerorExec(a) {
-	var b = [""];
-	return b.index = a.length,
-	b.input = a,
-	b
-}
-
-function sh_highlightString(a, b) {
-	var c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J;
-	if (/Konqueror/.test(navigator.userAgent) && !b.konquered) {
-		for (c = 0; c < b.length; c++)
-			for (d = 0; d < b[c].length; d++)
-				e = b[c][d][0],
-				"$" === e.source && (e.exec = sh_konquerorExec);
-		b.konquered = !0
+function sh_setHref(tags, numTags, inputString) {
+	var url = inputString.substring(tags[numTags - 2].pos, tags[numTags - 1].pos);
+	if (url.length >= 2 && url.charAt(0) === '<' && url.charAt(url.length - 1) === '>') {
+		url = url.substr(1, url.length - 2);
 	}
-	for (f = document.createElement("a"),
-	g = document.createElement("span"),
-	h = [],
-	i = 0,
-	j = [],
-	k = 0,
-	l = null,
-	m = function(b, c) {
-		var e, m, n, d = b.length;
-		0 !== d && (c || (e = j.length,
-		0 !== e && (m = j[e - 1],
-		m[3] || (c = m[1]))),
-		l !== c && (l && (h[i++] = {
-			pos: k
-		},
-		"sh_url" === l && sh_setHref(h, i, a)),
-		c && (n = "sh_url" === c ? f.cloneNode(!1) : g.cloneNode(!1),
-		n.className = c,
-		h[i++] = {
-			node: n,
-			pos: k
-		})),
-		k += d,
-		l = c)
+	if (sh_isEmailAddress(url)) {
+		url = 'mailto:' + url;
 	}
-	,
-	n = /\r\n|\r|\n/g,
-	n.lastIndex = 0,
-	o = a.length; o > k; ) {
-		for (p = k,
-		s = n.exec(a),
-		null === s ? (q = o,
-		r = o) : (q = s.index,
-		r = n.lastIndex),
-		t = a.substring(p, q),
-		u = []; ; ) {
-			for (v = k - p,
-			x = j.length,
-			w = 0 === x ? 0 : j[x - 1][2],
-			y = b[w],
-			z = y.length,
-			A = u[w],
-			A || (A = u[w] = []),
-			B = null,
-			C = -1,
-			D = 0; z > D && (D < A.length && (null === A[D] || v <= A[D].index) ? E = A[D] : (F = y[D][0],
-			F.lastIndex = v,
-			E = F.exec(t),
-			A[D] = E),
-			null === E || !(null === B || E.index < B.index) || (B = E,
-			C = D,
-			E.index !== v)); D++)
-				;
-			if (null === B) {
-				m(t.substring(v), null);
-				break
+	tags[numTags - 2].node.href = url;
+}
+
+/*
+Konqueror has a bug where the regular expression /$/g will not match at the end
+of a line more than once:
+
+	var regex = /$/g;
+	var match;
+
+	var line = '1234567890';
+	regex.lastIndex = 10;
+	match = regex.exec(line);
+
+	var line2 = 'abcde';
+	regex.lastIndex = 5;
+	match = regex.exec(line2);	// fails
+*/
+function sh_konquerorExec(s) {
+	var result = [''];
+	result.index = s.length;
+	result.input = s;
+	return result;
+}
+
+/**
+Highlights all elements containing source code in a text string.  The return
+value is an array of objects, each representing an HTML start or end tag.  Each
+object has a property named pos, which is an integer representing the text
+offset of the tag. Every start tag also has a property named node, which is the
+DOM element started by the tag. End tags do not have this property.
+@param  inputString	a text string
+@param  language  a language definition object
+@return	an array of tag objects
+*/
+function sh_highlightString(inputString, language) {
+	if (/Konqueror/.test(navigator.userAgent)) {
+		if (! language.konquered) {
+			for (var s = 0; s < language.length; s++) {
+				for (var p = 0; p < language[s].length; p++) {
+					var r = language[s][p][0];
+					if (r.source === '$') {
+						r.exec = sh_konquerorExec;
+					}
+				}
 			}
-			if (B.index > v && m(t.substring(v, B.index), null),
-			G = y[C],
-			H = G[1],
-			H instanceof Array)
-				for (J = 0; J < H.length; J++)
-				    I = B[J + 1],
-				    m(I, H[J]);
-			else
-				I = B[0],
-				m(I, H);
-			switch (G[2]) {
-			case -1:
-				break;
-			case -2:
-				j.pop();
-				break;
-			case -3:
-				j.length = 0;
-				break;
-			default:
-				j.push(G)
+			language.konquered = true;
+		}
+	}
+
+	var a = document.createElement('a');
+	var span = document.createElement('span');
+
+	// the result
+	var tags = [];
+	var numTags = 0;
+
+	// each element is a pattern object from language
+	var patternStack = [];
+
+	// the current position within inputString
+	var pos = 0;
+
+	// the name of the current style, or null if there is no current style
+	var currentStyle = null;
+
+	var output = function(s, style) {
+		var length = s.length;
+		// this is more than just an optimization - we don't want to output empty <span></span> elements
+		if (length === 0) {
+			return;
+		}
+		if (! style) {
+			var stackLength = patternStack.length;
+			if (stackLength !== 0) {
+				var pattern = patternStack[stackLength - 1];
+				// check whether this is a state or an environment
+				if (! pattern[3]) {
+					// it's not a state - it's an environment; use the style for this environment
+					style = pattern[1];
+				}
 			}
 		}
-		l && (h[i++] = {
-			pos: k
-		},
-		"sh_url" === l && sh_setHref(h, i, a),
-		l = null),
-		k = r
+		if (currentStyle !== style) {
+			if (currentStyle) {
+				tags[numTags++] = {pos: pos};
+				if (currentStyle === 'sh_url') {
+					sh_setHref(tags, numTags, inputString);
+				}
+			}
+			if (style) {
+				var clone;
+				if (style === 'sh_url') {
+					clone = a.cloneNode(false);
+				}
+				else {
+					clone = span.cloneNode(false);
+				}
+				clone.className = style;
+				tags[numTags++] = {node: clone, pos: pos};
+			}
+		}
+		pos += length;
+		currentStyle = style;
+	};
+
+	var endOfLinePattern = /\r\n|\r|\n/g;
+	endOfLinePattern.lastIndex = 0;
+	var inputStringLength = inputString.length;
+	while (pos < inputStringLength) {
+		var start = pos;
+		var end;
+		var startOfNextLine;
+		var endOfLineMatch = endOfLinePattern.exec(inputString);
+		if (endOfLineMatch === null) {
+			end = inputStringLength;
+			startOfNextLine = inputStringLength;
+		}
+		else {
+			end = endOfLineMatch.index;
+			startOfNextLine = endOfLinePattern.lastIndex;
+		}
+
+		var line = inputString.substring(start, end);
+
+		var matchCache = [];
+		for (;;) {
+			var posWithinLine = pos - start;
+
+			var stateIndex;
+			var stackLength = patternStack.length;
+			if (stackLength === 0) {
+				stateIndex = 0;
+			}
+			else {
+				// get the next state
+				stateIndex = patternStack[stackLength - 1][2];
+			}
+
+			var state = language[stateIndex];
+			var numPatterns = state.length;
+			var mc = matchCache[stateIndex];
+			if (! mc) {
+				mc = matchCache[stateIndex] = [];
+			}
+			var bestMatch = null;
+			var bestPatternIndex = -1;
+			for (var i = 0; i < numPatterns; i++) {
+				var match;
+				if (i < mc.length && (mc[i] === null || posWithinLine <= mc[i].index)) {
+					match = mc[i];
+				}
+				else {
+					var regex = state[i][0];
+					regex.lastIndex = posWithinLine;
+					match = regex.exec(line);
+					mc[i] = match;
+				}
+				if (match !== null && (bestMatch === null || match.index < bestMatch.index)) {
+					bestMatch = match;
+					bestPatternIndex = i;
+					if (match.index === posWithinLine) {
+						break;
+					}
+				}
+			}
+
+			if (bestMatch === null) {
+				output(line.substring(posWithinLine), null);
+				break;
+			}
+			else {
+				// got a match
+				if (bestMatch.index > posWithinLine) {
+					output(line.substring(posWithinLine, bestMatch.index), null);
+				}
+
+				var pattern = state[bestPatternIndex];
+
+				var newStyle = pattern[1];
+				var matchedString;
+				if (newStyle instanceof Array) {
+					for (var subexpression = 0; subexpression < newStyle.length; subexpression++) {
+						matchedString = bestMatch[subexpression + 1];
+						output(matchedString, newStyle[subexpression]);
+					}
+				}
+				else {
+					matchedString = bestMatch[0];
+					output(matchedString, newStyle);
+				}
+
+				switch (pattern[2]) {
+				case -1:
+					// do nothing
+					break;
+				case -2:
+					// exit
+					patternStack.pop();
+					break;
+				case -3:
+					// exitall
+					patternStack.length = 0;
+					break;
+				default:
+					// this was the start of a delimited pattern or a state/environment
+					patternStack.push(pattern);
+					break;
+				}
+			}
+		}
+
+		// end of the line
+		if (currentStyle) {
+			tags[numTags++] = {pos: pos};
+			if (currentStyle === 'sh_url') {
+				sh_setHref(tags, numTags, inputString);
+			}
+			currentStyle = null;
+		}
+		pos = startOfNextLine;
 	}
-	return h
+
+	return tags;
 }
 
-function sh_getClasses(a) {
-	var d, e, b = [], c = a.className;
-	if (c && c.length > 0)
-		for (d = c.split(" "),
-		e = 0; e < d.length; e++)
-			d[e].length > 0 && b.push(d[e]);
-	return b
+////////////////////////////////////////////////////////////////////////////////
+// DOM-dependent functions
+
+function sh_getClasses(element) {
+	var result = [];
+	var htmlClass = element.className;
+	if (htmlClass && htmlClass.length > 0) {
+		var htmlClasses = htmlClass.split(' ');
+		for (var i = 0; i < htmlClasses.length; i++) {
+			if (htmlClasses[i].length > 0) {
+				result.push(htmlClasses[i]);
+			}
+		}
+	}
+	return result;
 }
 
-function sh_extractTagsFromNodeList(a, b) {
-	var d, e, f, c = a.length;
-	for (d = 0; c > d; d++)
-		switch (e = a.item(d),
-		e.nodeType) {
+function sh_addClass(element, name) {
+	var htmlClasses = sh_getClasses(element);
+	for (var i = 0; i < htmlClasses.length; i++) {
+		if (name.toLowerCase() === htmlClasses[i].toLowerCase()) {
+			return;
+		}
+	}
+	htmlClasses.push(name);
+	element.className = htmlClasses.join(' ');
+}
+
+/**
+Extracts the tags from an HTML DOM NodeList.
+@param  nodeList  a DOM NodeList
+@param  result  an object with text, tags and pos properties
+*/
+function sh_extractTagsFromNodeList(nodeList, result) {
+	var length = nodeList.length;
+	for (var i = 0; i < length; i++) {
+		var node = nodeList.item(i);
+		switch (node.nodeType) {
 		case 1:
-			"br" === e.nodeName.toLowerCase() ? (f = /MSIE/.test(navigator.userAgent) ? "\r" : "\n",
-			b.text.push(f),
-			b.pos++) : (b.tags.push({
-				node: e.cloneNode(!1),
-				pos: b.pos
-			}),
-			sh_extractTagsFromNodeList(e.childNodes, b),
-			b.tags.push({
-				pos: b.pos
-			}));
+			if (node.nodeName.toLowerCase() === 'br') {
+				var terminator;
+				if (/MSIE/.test(navigator.userAgent)) {
+					terminator = '\r';
+				}
+				else {
+					terminator = '\n';
+				}
+				result.text.push(terminator);
+				result.pos++;
+			}
+			else {
+				result.tags.push({node: node.cloneNode(false), pos: result.pos});
+				sh_extractTagsFromNodeList(node.childNodes, result);
+				result.tags.push({pos: result.pos});
+			}
 			break;
 		case 3:
 		case 4:
-			b.text.push(e.data),
-			b.pos += e.length
+			result.text.push(node.data);
+			result.pos += node.length;
+			break;
 		}
+	}
 }
 
-function sh_extractTags(a, b) {
-	var c = {};
-	return c.text = [],
-	c.tags = b,
-	c.pos = 0,
-	sh_extractTagsFromNodeList(a.childNodes, c),
-	c.text.join("")
+/**
+Extracts the tags from the text of an HTML element. The extracted tags will be
+returned as an array of tag objects. See sh_highlightString for the format of
+the tag objects.
+@param  element	a DOM element
+@param  tags  an empty array; the extracted tag objects will be returned in it
+@return  the text of the element
+@see  sh_highlightString
+*/
+function sh_extractTags(element, tags) {
+	var result = {};
+	result.text = [];
+	result.tags = tags;
+	result.pos = 0;
+	sh_extractTagsFromNodeList(element.childNodes, result);
+	return result.text.join('');
 }
 
-function sh_mergeTags(a, b) {
-	var d, e, f, g, h, i, c = a.length;
-	if (0 === c)
-		return b;
-	if (d = b.length,
-	0 === d)
-		return a;
-	for (e = [],
-	f = 0,
-	g = 0; c > f && d > g; )
-		h = a[f],
-		i = b[g],
-		h.pos <= i.pos ? (e.push(h),
-		f++) : (e.push(i),
-		b[g + 1].pos <= h.pos ? (g++,
-		e.push(b[g]),
-		g++) : (e.push({
-			pos: h.pos
-		}),
-		b[g] = {
-			node: i.node.cloneNode(!1),
-			pos: h.pos
-		}));
-	for (; c > f; )
-		e.push(a[f]),
-		f++;
-	for (; d > g; )
-		e.push(b[g]),
-		g++;
-	return e
+/**
+Merges the original tags from an element with the tags produced by highlighting.
+@param	originalTags	an array containing the original tags
+@param	highlightTags	an array containing the highlighting tags - these must not overlap
+@result	an array containing the merged tags
+*/
+function sh_mergeTags(originalTags, highlightTags) {
+	var numOriginalTags = originalTags.length;
+	if (numOriginalTags === 0) {
+		return highlightTags;
+	}
+
+	var numHighlightTags = highlightTags.length;
+	if (numHighlightTags === 0) {
+		return originalTags;
+	}
+
+	var result = [];
+	var originalIndex = 0;
+	var highlightIndex = 0;
+
+	while (originalIndex < numOriginalTags && highlightIndex < numHighlightTags) {
+		var originalTag = originalTags[originalIndex];
+		var highlightTag = highlightTags[highlightIndex];
+
+		if (originalTag.pos <= highlightTag.pos) {
+			result.push(originalTag);
+			originalIndex++;
+		}
+		else {
+			result.push(highlightTag);
+			if (highlightTags[highlightIndex + 1].pos <= originalTag.pos) {
+				highlightIndex++;
+				result.push(highlightTags[highlightIndex]);
+				highlightIndex++;
+			}
+			else {
+				// new end tag
+				result.push({pos: originalTag.pos});
+
+				// new start tag
+				highlightTags[highlightIndex] = {node: highlightTag.node.cloneNode(false), pos: originalTag.pos};
+			}
+		}
+	}
+
+	while (originalIndex < numOriginalTags) {
+		result.push(originalTags[originalIndex]);
+		originalIndex++;
+	}
+
+	while (highlightIndex < numHighlightTags) {
+		result.push(highlightTags[highlightIndex]);
+		highlightIndex++;
+	}
+
+	return result;
 }
 
-function sh_insertTags(a, b) {
-	for (var j, k, l, c = document, d = document.createDocumentFragment(), e = 0, f = a.length, g = 0, h = b.length, i = d; h > g || f > e; )
-		f > e ? (j = a[e],
-		k = j.pos) : k = h,
-		g >= k ? (j.node ? (l = j.node,
-		i.appendChild(l),
-		i = l) : i = i.parentNode,
-		e++) : (i.appendChild(c.createTextNode(b.substring(g, k))),
-		g = k);
-	return d
+/**
+Inserts tags into text.
+@param  tags  an array of tag objects
+@param  text  a string representing the text
+@return	a DOM DocumentFragment representing the resulting HTML
+*/
+function sh_insertTags(tags, text) {
+	var doc = document;
+
+	var result = document.createDocumentFragment();
+	var tagIndex = 0;
+	var numTags = tags.length;
+	var textPos = 0;
+	var textLength = text.length;
+	var currentNode = result;
+
+	// output one tag or text node every iteration
+	while (textPos < textLength || tagIndex < numTags) {
+		var tag;
+		var tagPos;
+		if (tagIndex < numTags) {
+			tag = tags[tagIndex];
+			tagPos = tag.pos;
+		}
+		else {
+			tagPos = textLength;
+		}
+
+		if (tagPos <= textPos) {
+			// output the tag
+			if (tag.node) {
+				// start tag
+				var newNode = tag.node;
+				currentNode.appendChild(newNode);
+				currentNode = newNode;
+			}
+			else {
+				// end tag
+				currentNode = currentNode.parentNode;
+			}
+			tagIndex++;
+		}
+		else {
+			// output text
+			currentNode.appendChild(doc.createTextNode(text.substring(textPos, tagPos)));
+			textPos = tagPos;
+		}
+	}
+
+	return result;
 }
 
-function sh_highlightElement(a, b) {
-	var c, d, e, f, g;
-	for (c = [],
-	d = sh_extractTags(a, c),
-	e = sh_highlightString(d, b),
-	f = sh_mergeTags(c, e),
-	g = sh_insertTags(f, d); a.hasChildNodes(); )
-		a.removeChild(a.firstChild);
-	a.appendChild(g)
+/**
+Highlights an element containing source code.  Upon completion of this function,
+the element will have been placed in the "sh_sourceCode" class.
+@param  element	a DOM <pre> element containing the source code to be highlighted
+@param  language  a language definition object
+*/
+function sh_highlightElement(element, language) {
+	var originalTags = [];
+	var inputString = sh_extractTags(element, originalTags);
+	var highlightTags = sh_highlightString(inputString, language);
+	var tags = sh_mergeTags(originalTags, highlightTags);
+	var documentFragment = sh_insertTags(tags, inputString);
+	while (element.hasChildNodes()) {
+		element.removeChild(element.firstChild);
+	}
+	element.appendChild(documentFragment);
 }
 
 function syntax_highlight() {
-	var c, d, e, f, h, b = document.getElementsByTagName("code");
-	for(c = 0; c < b.length; c++){
-		d = b.item(c);
-		e = sh_getClasses(d);
-		for(f = 0; f < e.length; f++){
-			if (e[f].toLowerCase() == 'sh_cpp') {
-				sh_highlightElement(d, syntax_highlight_cpp);
+	var nodeList = document.getElementsByTagName('code');
+	for (var i = 0; i < nodeList.length; i++) {
+		var element = nodeList.item(i);
+		var htmlClasses = sh_getClasses(element);
+		for (var j = 0; j < htmlClasses.length; j++)
+			if (htmlClasses[j].toLowerCase() === 'sh_cpp') {
+				sh_highlightElement(element, syntax_highlight_cpp);
 				break;
 			}
-		}
 	}
 }
--- additional_files/sha1.js
+++ additional_files/sha1.js
@@ -1,68 +1,21 @@
-/*   
- *   A   JavaScript   implementation   of   the   Secure   Hash   Algorithm,   SHA-1,   as   defined   
- *   in   FIPS   PUB   180-1   
- *   Version   2.1-BETA   Copyright   Paul   Johnston   2000   -   2002.   
- *   Other   contributors:   Greg   Holt,   Andrew   Kepert,   Ydnar,   Lostinet   
- *   Distributed   under   the   BSD   License   
- *   See   http://pajhome.org.uk/crypt/md5   for   details.   
- */
-/*   
- *   Configurable   variables.   You   may   need   to   tweak   these   to   be   compatible   with   
- *   the   server-side,   but   the   defaults   work   in   most   cases.   
- */
-var hexcase = 0; /*   hex   output   format.   0   -   lowercase;   1   -   uppercase                 */
-var b64pad = ""; /*   base-64   pad   character.   "="   for   strict   RFC   compliance       */
-var chrsz = 8; /*   bits   per   input   character.   8   -   ASCII;   16   -   Unicode             */
-
-/*   
- *   These   are   the   functions   you'll   usually   want   to   call   
- *   They   take   string   arguments   and   return   either   hex   or   base-64   encoded   strings   
- */
 function hex_sha1(s) {
-    return binb2hex(core_sha1(str2binb(s), s.length * chrsz));
-}
-
-function b64_sha1(s) {
-    return binb2b64(core_sha1(str2binb(s), s.length * chrsz));
+    return binb2hex(core_sha1(str2binb(s), s.length << 3));
 }
 
 function str_sha1(s) {
-    return binb2str(core_sha1(str2binb(s), s.length * chrsz));
+    return binb2str(core_sha1(str2binb(s), s.length << 3));
 }
 
-function hex_hmac_sha1(key, data) {
-    return binb2hex(core_hmac_sha1(key, data));
-}
-
-function b64_hmac_sha1(key, data) {
-    return binb2b64(core_hmac_sha1(key, data));
-}
-
-function str_hmac_sha1(key, data) {
-    return binb2str(core_hmac_sha1(key, data));
-}
-
-/*   
- *   Perform   a   simple   self-test   to   see   if   the   VM   is   working   
- */
-function sha1_vm_test() {
-    return hex_sha1("abc") == "a9993e364706816aba3e25717850c26c9cd0d89d";
-}
-
-/*   
- *   Calculate   the   SHA-1   of   an   array   of   big-endian   words,   and   a   bit   length   
- */
 function core_sha1(x, len) {
-    /*   append   padding   */
-    x[len >> 5] |= 0x80 << (24 - len % 32);
+    x[len >> 5] |= 0x80 << (24 - len & 24);
     x[((len + 64 >> 9) << 4) + 15] = len;
 
     var w = Array(80);
-    var a = 1732584193;
-    var b = -271733879;
-    var c = -1732584194;
-    var d = 271733878;
-    var e = -1009589776;
+    var a = 0x67452301;
+    var b = 0xefcdab89;
+    var c = 0x98badcfe;
+    var d = 0x10325476;
+    var e = 0xc3d2e1f0;
 
     for (var i = 0; i < x.length; i += 16) {
         var olda = a;
@@ -92,10 +45,6 @@ function core_sha1(x, len) {
 
 }
 
-/*   
- *   Perform   the   appropriate   triplet   combination   function   for   the   current   
- *   iteration   
- */
 function sha1_ft(t, b, c, d) {
     if (t < 20) return (b & c) | ((~b) & d);
     if (t < 40) return b ^ c ^ d;
@@ -103,95 +52,44 @@ function sha1_ft(t, b, c, d) {
     return b ^ c ^ d;
 }
 
-/*   
- *   Determine   the   appropriate   additive   constant   for   the   current   iteration   
- */
 function sha1_kt(t) {
-    return (t < 20) ? 1518500249 : (t < 40) ? 1859775393 : (t < 60) ? -1894007588 : -899497514;
+    return (t < 20) ? 0x5a827999 : (t < 40) ? 0x6ed9eba1 : (t < 60) ? 0x8f1bbcdc : 0xca62c1d6;
 }
 
-/*   
- *   Calculate   the   HMAC-SHA1   of   a   key   and   some   data   
- */
-function core_hmac_sha1(key, data) {
-    var bkey = str2binb(key);
-    if (bkey.length > 16) bkey = core_sha1(bkey, key.length * chrsz);
-
-    var ipad = Array(16),
-        opad = Array(16);
-    for (var i = 0; i < 16; i++) {
-        ipad[i] = bkey[i] ^ 0x36363636;
-        opad[i] = bkey[i] ^ 0x5C5C5C5C;
-    }
-
-    var hash = core_sha1(ipad.concat(str2binb(data)), 512 + data.length * chrsz);
-    return core_sha1(opad.concat(hash), 512 + 160);
-}
-
-/*   
- *   Add   integers,   wrapping   at   2^32.   This   uses   16-bit   operations   internally   
- *   to   work   around   bugs   in   some   JS   interpreters.   
- */
 function safe_add(x, y) {
     var lsw = (x & 0xFFFF) + (y & 0xFFFF);
     var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
     return (msw << 16) | (lsw & 0xFFFF);
 }
 
-/*   
- *   Bitwise   rotate   a   32-bit   number   to   the   left.   
- */
 function rol(num, cnt) {
     return (num << cnt) | (num >>> (32 - cnt));
 }
 
-/*   
- *   Convert   an   8-bit   or   16-bit   string   to   an   array   of   big-endian   words   
- *   In   8-bit   function,   characters   >255   have   their   hi-byte   silently   ignored.   
- */
 function str2binb(str) {
-    var bin = Array();
-    var mask = (1 << chrsz) - 1;
-    for (var i = 0; i < str.length * chrsz; i += chrsz)
-    bin[i >> 5] |= (str.charCodeAt(i / chrsz) & mask) << (24 - i % 32);
+	var bin = [], i;
+	for (i = 0; i < str.length * 8; i += 8)
+		bin[i >> 5] |= (str.charCodeAt(i >> 3) & 255) << (24 - i & 24);
     return bin;
 }
 
-/*   
- *   Convert   an   array   of   big-endian   words   to   a   string   
- */
 function binb2str(bin) {
-    var str = "";
-    var mask = (1 << chrsz) - 1;
-    for (var i = 0; i < bin.length * 32; i += chrsz)
-    str += String.fromCharCode((bin[i >> 5] >>> (24 - i % 32)) & mask);
+	var str = '', i;
+	for (i = 0; i < bin.length * 32; i += 8)
+		str += String.fromCharCode(bin[i >> 5] >>> (24 - i & 24) & 255);
+	return str;
+}
+
+function binb2hex(bin) {
+    var hex_tab = '0123456789abcdef', str = '', i;
+    for (i = 0; i < bin.length * 8; ++i)
+        str += hex_tab[(bin[i >> 3] >> ((7 - i & 7) * 4)) & 15];
     return str;
 }
 
-/*   
- *   Convert   an   array   of   big-endian   words   to   a   hex   string.   
- */
-function binb2hex(binarray) {
-    var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
-    var str = "";
-    for (var i = 0; i < binarray.length * 4; i++) {
-        str += hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8 + 4)) & 0xF) + hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8)) & 0xF);
-    }
-    return str;
-}
-
-/*   
- *   Convert   an   array   of   big-endian   words   to   a   base-64   string   
- */
-function binb2b64(binarray) {
-    var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    var str = "";
-    for (var i = 0; i < binarray.length * 4; i += 3) {
-        var triplet = (((binarray[i >> 2] >> 8 * (3 - i % 4)) & 0xFF) << 16) | (((binarray[i + 1 >> 2] >> 8 * (3 - (i + 1) % 4)) & 0xFF) << 8) | ((binarray[i + 2 >> 2] >> 8 * (3 - (i + 2) % 4)) & 0xFF);
-        for (var j = 0; j < 4; j++) {
-            if (i * 8 + j * 6 > binarray.length * 32) str += b64pad;
-            else str += tab.charAt((triplet >> 6 * (3 - j)) & 0x3F);
-        }
-    }
-    return str;
+function hex2binb(str) {
+	var hex_tab = {0:0,1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,a:10,b:11,c:12,d:13,e:14,f:15}, bin = [], i;
+	for (i = 0; i < str.length; ++i)
+		bin[i >> 3] |= hex_tab[str[i]] << ((7 - i & 7) * 4);
+	return bin;
 }
